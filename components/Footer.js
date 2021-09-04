4628881ad4ce9c022f0553bfe65932db781c87c1
Gechrist <gchris@hotmail.co.uk>
2021-09-04 16:32:24 +0300

    Fixed github link in footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ import Image from 'next/image'
 const Footer = () => {
     return (
             <div className="w-full lg:w-main p-2 lg:ml-menu h-auto bg-black text-center absolute bottom-0">
-                <a className="text-sm md:text-base hover:font-bold" rel="noreferrer" href="https://github.com/Gechrist/imdb-nextjs" target="_blank">Github repository</a>
+                <a className="text-sm md:text-base hover:font-bold" rel="noreferrer" href="https://github.com/Gechrist/NextJS---Imdb" target="_blank">Github repository</a>
                 <p>This product uses the TMDb API but is not endorsed or certified by TMDb</p>
                 <div className="relative h-2 md:h-6"><Image aria-label="Tmdb logo" src = {Tmdb} layout='fill' objectFit="contain" alt='tmdb logo'/></div>
             </div>

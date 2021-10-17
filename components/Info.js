77a9f3ac479cf4c3e6548cb6cc96c4a33b0f0e5d
Gechrist <gchris@hotmail.co.uk>
2021-10-17 11:35:51 +0300

    media info box wrong width fix

--- components/Info.js
+++ components/Info.js
@@ -1,7 +1,7 @@
 
 const Info = ({info}) => {
     return (info?
-        <article className=" border-2 border-yellow-300 h-72 overflow-y-auto absolute p-10 whitespace-normal 
+        <article className=" border-2 border-yellow-300 h-72 w-full overflow-y-auto absolute p-10 whitespace-normal 
         bg-black text-xl bottom-12 md:bottom-14 xl:bottom-18 text-xl 2xl:bottom-26">
             <p>{info}</p>
         </article>:<p className="absolute bottom-16 text-xl"> No Info Available</p>

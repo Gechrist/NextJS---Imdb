1054ef968ec5c46f61cb62e6ce8e7e5b58d9f5ed
Gechrist <gchris@hotmail.co.uk>
2021-10-17 12:11:22 +0300

    Layout fix

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from '../components/Footer';
-import Head from 'next/head'
 
 const Layout =({children}) => {
     return(
-        <>
         <div>
         <Header/>
         <main> {children}</main>
         <Footer/>
         </div>
-        </> 
     )
 
 }
